c1cf0597a89374ada160e4a9209c7201ac4735cf
Anatolii Karlov <karleowne@gmail.com>
2026-08-16 23:09:17 +0700

    fix: allow trusted remote cover images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co",
+        port: "",
+        pathname: "/file/anilistcdn/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "covers.openlibrary.org",
+        port: "",
+        pathname: "/b/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
